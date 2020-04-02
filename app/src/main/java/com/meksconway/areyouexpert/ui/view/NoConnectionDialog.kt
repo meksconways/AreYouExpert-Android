92ed5c921685a47cca5211fb5b908752d44768fd
nuriefeoglu <nuriefeoglu09@gmail.com>
2020-04-03 02:23:33 +0300

    custom dialog popup geliştirmesi

---
+++ app/src/main/java/com/meksconway/areyouexpert/ui/view/NoConnectionDialog.kt
@@ -0,0 +1,13 @@
+package com.meksconway.areyouexpert.ui.view
+
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import com.meksconway.areyouexpert.R
+
+class NoConnectionDialog(context: Context) : Dialog(context) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.)
+    }
+}
