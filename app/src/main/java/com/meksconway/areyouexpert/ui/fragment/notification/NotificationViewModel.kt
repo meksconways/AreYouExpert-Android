49905dad58fd1e2b851df8ddeeb74755942bdedb
nuriefeoglu <nuriefeoglu09@gmail.com>
2020-04-06 13:13:22 +0300

    notification fragment xml düzenlendi

---
+++ app/src/main/java/com/meksconway/areyouexpert/ui/fragment/notification/NotificationViewModel.kt
@@ -0,0 +1,7 @@
+package com.meksconway.areyouexpert.ui.fragment.notification
+
+import androidx.lifecycle.ViewModel
+
+class NotificationViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
