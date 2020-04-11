4ea1fafbda61bc6c2c689416bd418b1edd2ae520
meksconways <muhammedeminkisi@gmail.com>
2020-04-12 01:49:01 +0300

    task ok

--- app/src/main/java/com/meksconway/areyouexpert/ui/view/ResetProgressDialog.kt
+++ app/src/main/java/com/meksconway/areyouexpert/ui/view/ResetProgressDialog.kt
@@ -6,13 +6,13 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import com.meksconway.areyouexpert.R
-import kotlinx.android.synthetic.main.custom_dialog_drop_db.*
+import kotlinx.android.synthetic.main.dialog_reset_progress.*
 
 class DropDatabaseDialog(context: Context, private val callback: (Boolean) -> Unit) : Dialog(context) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.custom_dialog_drop_db)
+        setContentView(R.layout.dialog_reset_progress)
         window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         btnYes.setOnClickListener{
             callback.invoke(true)
